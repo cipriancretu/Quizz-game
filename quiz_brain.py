594ca3179385356948778eba09bcffe71734120a
Ciprian Cretu <124049021+cipriancretu@users.noreply.github.com>
2023-05-12 14:59:24 +0100

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,4 @@
+"""Import HTML module to unescape the text"""
 import html
 
 
@@ -8,16 +9,16 @@ class QuizBrain:
         self.score = 0
         self.question_list = q_list
         self.current_question = None
-
+    """ Function check if are questions left"""
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
-
+    """Function return the next question"""
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
-
+    """ Function check the answer if is correct"""
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
