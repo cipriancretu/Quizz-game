76517cdde11470a2d15a31227a7782b77402c5e3
Ciprian Cretu <124049021+cipriancretu@users.noreply.github.com>
2023-05-12 14:13:36 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
+
+"""Import requests module"""
 import requests
 
 parameters = {
     "amount": 10,
     "type": "boolean",
 }
-
+"""Get the questions and results"""
 response = requests.get("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=boolean", params=parameters)
 response.raise_for_status()
 data = response.json()
